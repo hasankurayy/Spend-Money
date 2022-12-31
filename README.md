61bb3199ac3e479930e502f0007e3173a60ee896
Hasan Kuray <69965179+hasankurayy@users.noreply.github.com>
2022-12-31 17:58:27 +0300

    added logo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Run The Project
 
-To run the project click the [link](https://spendmoneyhkurayy.surge.sh)
+To run the project click the [link](https://spendmoneyhkry.surge.sh)
 
 ## Description
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import React from 'react'
 function Header({ money, total }) {
     return (
         <div className='header-main'>
+            <div className="logo-mine">
+                <a href="#">HKRY</a>
+            </div>
+
             {money - total > 0 && total !== 0 && (
                 <div className='header'>
                     Harcayacak <span>{money - total} TL</span> paranız kaldı.
@@ -20,6 +24,8 @@ function Header({ money, total }) {
                     Paran bitti, parasız insan boş insandır!
                 </div>
             )}
+
+            <div></div>
 
         </div >
     )
